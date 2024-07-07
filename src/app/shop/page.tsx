e5cbc26e935fb52b9db19d7b90e718dd8a3bf94b
Nayan1234l <ali15-13943@diu.edu.bd>
2024-07-07 15:05:53 +0600

    fix

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,22 +1,23 @@
 "use client";
 import React from "react";
-import menu from "../../../public/data/menu";
-import Image from "next/image";
+import { useRouter } from "next/router";
 import Resturant from "@/components/Restaurant";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-// import image '../../../public/images'
 
 const Shop = () => {
+  const router = useRouter();
+
   return (
     <>
-      <Header pathname={window.location.pathname} />
+      <Header pathname={router.pathname} />
       <div className="container mx-auto px-4 py-8 bg-ora-400">
         <h1 className="text-3xl font-semibold mb-6"></h1>
         <p className="text-lg">
+          {/* Add your content here */}
         </p>
         <p className="text-lg mt-4">
-      <Resturant />
+          <Resturant />
         </p>
         {/* Add more content about your company */}
       </div>
